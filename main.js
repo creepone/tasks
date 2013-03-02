38be6106347eccbd35067503005f3f9dd2180b94
Tomas Vana <tomas@Tomas-Vanas-iMac.local>
2013-03-02 19:08:57 +0100

    quickfix

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var authentication = require('private/authentication');
+var authentication = require('./private/authentication');
 
 app.use(express.static(__dirname + '/public'));
 
