fb25ac54e605d0cc0d23c28401706b80a538750e
Tomas Vana <tomas@Tomas-Vanas-iMac.local>
2013-03-02 18:49:29 +0100

    authentication stub

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
 
+var authentication = require('private/authentication');
+
 app.use(express.static(__dirname + '/public'));
+
+app.get('/ios/authenticate', authentication.authenticateDevice);
+
 app.listen(process.env.PORT || 8081);
---
+++ private/authentication.js
@@ -0,0 +1,22 @@
+var openid = require('openid');
+
+function authenticateDevice(req, res) 
+{
+	var relyingParty = new openid.RelyingParty('http://tasks.iosapps.at/ios/verify', null, false, false, []);
+	relyingParty.authenticate(o.openid, false, function(error, authUrl)
+	{
+		if (error || !authUrl)
+		{
+			// todo: show error page with a close button so that the user can exit the process
+			res.writeHead(200);
+			res.end('Authentication failed');
+		}
+		else
+		{
+			res.writeHead(302, { Location: authUrl });
+			res.end();
+		}
+	});
+}
+
+exports.authenticateDevice = authenticateDevice;
